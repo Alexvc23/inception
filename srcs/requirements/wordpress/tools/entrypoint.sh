84c4644d2514fb210f684ea7d95b856e4a8ef564
Alexvc23 <alexandervalencia1994@gmail.com>
2023-02-23 15:13:25 +0100

    update

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,27 +1,16 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    entrypoint.sh                                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: alexandervalencia <alexandervalencia@st    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/02/13 11:24:28 by alexanderva       #+#    #+#              #
-#    Updated: 2023/02/13 17:00:11 by alexanderva      ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 #!/bin/bash
 
-if ! wp core verify-checksums 
+# WORDPRESS SITE SETUP
+cd /var/www/html/wordpress
+if ! wp core is-installed --allow-root
 then
-    wp core install --allow-root \
-    --urs=$DOMAIN_NAME \
-    --title=$WP_SITE_TITLE \
-    --admin_user=$WP_ADMIN \
-    --admin_password=$WP_ADMIN_PASSWORD \
-    --admin_email=$WP_ADMIN_EMAIL
+	wp core install	--allow-root \
+					--url=$DOMAIN_NAME \
+					--title="$WP_SITE_TITLE" \
+					--admin_user=$WP_ADMIN \
+					--admin_password=$WP_ADMIN_PASSWORD \
+					--admin_email=$WP_ADMIN_EMAIL
 fi
 
-# launch php-fpm as a daemon in the foregroud this will listening for CGI
-# requests 
-php-fpm7.4
+# LAUNCH PHP-FPM
+php-fpm7.3 -F
